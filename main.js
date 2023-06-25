ede129650f55f5f70bbd44e360c07adc4808f5f6
test <pikachu>
2023-06-25 19:50:15 +0900

    first

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log(handTrack);
+
+const video = document.getElementById("myvideo");
+const canvas = document.getElementById("mycanvas");
+
+let model;
+
+const options = {
+    flipHorizontal:false,
+    maxNumBoxes:3,
+    scoreThreshold:0.7,
+}
+
+let context = canvas.getContext("2d");
+
+handTrack.load(options).then(function(modelData){
+    model = modelData;
+
+    //webカメラを起動する
+    handTrack.startVideo(video).then((status) => {
+        if(status){
+            console.log(status);
+            startDetection();
+        }
+        else{
+            console.log("fail");
+        }
+    });
+});
+
+function startDetection(){
+    model.detect(video).then((predictions) => {
+        model.renderPredictions(predictions,canvas,context,video);
+
+        requestAnimationFrame(startDetection);
+    })
+}
